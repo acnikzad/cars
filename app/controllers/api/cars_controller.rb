8349b5f60612e25d8faaec45085bd986858ad3a2
“Aria <a.c.nikzad@gmail.com>
2022-02-06 10:19:32 -0800

    finished controlelr crud

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -13,8 +13,28 @@ class Api::CarsController < ApplicationController
       submodel: params[:submodel],
       color: params[:color],
       year: params[:year],
-
       )
+  end
 
+  def show
+    @cars = Car.find_by(id:params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @cars = car.find_by(id:params[:id])
+    @car.make = params[:make] || @car.make
+    @car.model = params[:model] || @car.model
+    @car.submodel = params[:submodel] || @car.submodel
+    @car.color = params[:color] || @car.color
+    @car.year = params[:year] || @car.year
+    @car.save
+  end
+
+  def destroy
+    car = Car.find_by(id:params[:id])
+    car.destroy
+    render json: {message:"Car has been successfuly destroyed"}
+  end
 
 end
