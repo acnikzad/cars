5fce89276f3095956f5333350844c456a0499b4c
“Aria <a.c.nikzad@gmail.com>
2022-02-17 14:41:31 -0800

    changed to heroku 18

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'cars-fe.herokuapp.com/'
+    origins 'localhost:8080', 'cars-fe.herokuapp.com',
 
     resource '*',
       headers: :any,
