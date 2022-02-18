dc175195c08004a36a717bfd18db79f714689eab
“Aria <a.c.nikzad@gmail.com>
2022-02-17 18:44:06 -0800

    added CORS lines

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,12 +9,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080', 'cars-fe.herokuapp.com';
 
-    resource '/cars',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-
-    # resource '*',
+    # resource '/cars',
     #   headers: :any,
     #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
